b8bc89aa43b50898f2ea7b7db3c52ae9ba1bcc72
TommyRizaldi <ach.rizaldiutomo@gmail.com>
2024-08-25 11:59:03 +0700

    chore(config): add Tailwind configuration

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdex}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdex}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdex}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
