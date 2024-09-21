7f85674d5e1726b3d37bb8f7b75f266f59d3b0d7
TommyRizaldi <ach.rizaldiutomo@gmail.com>
2024-09-21 22:37:14 +0700

    chore(config): add login and regis

--- apps/web/src/components/Login.tsx
+++ apps/web/src/components/Login.tsx
@@ -1,100 +1,49 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-export default function Example() {
-    return (
-      <>
-        {/*
-          This example requires updating your template:
-  
-          ```
-          <html class="h-full bg-white">
-          <body class="h-full">
-          ```
-        */}
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              alt="Your Company"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              className="mx-auto h-10 w-auto"
-            />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
-            </h2>
-          </div>
-  
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form action="#" method="POST" className="space-y-6">
-              <div>
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    required
-                    autoComplete="email"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                    Password
-                  </label>
-                  <div className="text-sm">
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                      Forgot password?
-                    </a>
-                  </div>
-                </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    required
-                    autoComplete="current-password"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign in
-                </button>
-              </div>
-            </form>
-  
-            <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{' '}
-              <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                Start a 14 day free trial
-              </a>
-            </p>
-          </div>
-        </div>
-      </>
+"use client"
+import { ErrorMessage, Field, Form, Formik, FormikHelpers } from 'formik';
+import * as yup from 'yup'
+
+const LoginSchema= yup.object().shape({
+    email: yup.string().email("invalid email").required("email required"),
+    password: yup.string()
+        .min(6, "password must be at least 6 characters")
+        .required("password required")
+});
+
+export default function LoginForm() {
+    return(
+        <Formik
+            initialValues={{
+                email: "",
+                password: ""
+            }}
+            validationSchema={LoginSchema}
+            onSubmit={(values, action) => {}}
+        >
+            {
+                () => {
+                    return (
+                        <Form>
+                            <div className=" min-w-[30vw]">
+                                <div className="mt-10">
+                                    <label className="block text-sm font-medium leading-8 text-white">Email</label>
+                                    <div className="mt-2">
+                                        <Field name="email" type="text" className="block w-full rounded-md border-0 p-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                        <ErrorMessage name="email" component={"div"} className="text-sm text-red-500"/>
+                                    </div>
+                                </div>
+                                <div>
+                                    <label className="block text-sm font-medium leading-8 text-white">Password</label>
+                                    <div className="mt-2">
+                                        <Field name="password" type="password" className="block w-full rounded-md border-0 p-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                        <ErrorMessage name="password" component={"div"} className="text-sm text-red-500"/>
+                                    </div>
+                                </div>
+                                <button type="submit" className="w-full mt-6 p-1.5 text-sm font-medium rounded-md bg-orange-500 ">Login</button>
+                            </div>
+                        </Form>
+                    )
+                }
+            }
+        </Formik>
     )
-  }
-  
+}
